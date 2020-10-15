0bd452df2fe00f136a2826b99bcb8fbc5f8f0c4f
vitaly <vgrudachev.exceedteam@gmail.com>
2020-10-15 15:51:18 +0300

    fixed serch-string after testing

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,11 +1,11 @@
 function searchString(obj) {
   let str = '?';
   for (let key in obj) {    
-    if (obj[key] != null && obj[key] != undefined && obj[key] != '') {
+    if (obj[key] != undefined && obj[key] !== '') {
       str = str + (key + '=' + obj[key] + '&');
     }
   }
-  return str.slice(0, str.length - 2);
+  return str.slice(0, str.length - 1);
 }
 
 window.searchString = searchString;
