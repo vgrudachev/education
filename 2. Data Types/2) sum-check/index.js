a98535f4e5f309f440e7fe8fb53b4517b041991c
vitaly <vgrudachev.exceedteam@gmail.com>
2020-10-15 13:49:26 +0300

    complite sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,6 +1,15 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+  try {
+    if (typeof +a === 'number' && typeof +b === 'number' && !isNaN(a) && !isNaN(b)) {
+      return +a + +b;
+    }
+    throw new Error("Wrong arguments type!");
+  } catch(e) {
+    alert(e.message);
+  }
 }
+
+sumCheck(2, 'b')
 
 window.sumCheck = sumCheck;
 
