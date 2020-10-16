35623a1f491dba890fe14bcdc3520998ed7efaf5
vitaly <vgrudachev.exceedteam@gmail.com>
2020-10-16 11:47:14 +0300

    complete is-equal-simple after testing

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,6 +1,48 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
-}
+  if ( a == null || b == null ) {
+    return false;
+  }
+
+  let keysa = Object.keys(a);
+  let keysb = Object.keys(b);
+  let length = 0;
+
+  let vala = Object.values(a);
+  let valb = Object.values(b);
+
+  let rightNum = 0;
+
+  if (keysa.length > keysb.length || keysa.length < keysb.length) {
+    return false;
+  } else {
+    length = keysa.length;
+  }
+
+  for (let i = 0; i <= length - 1; i++) {
+    if (keysa[i] === keysb[i] && vala[i] === valb[i]) {
+      rightNum++;
+    }
+    continue;
+  } 
+
+  if (rightNum == length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+let aa = {
+  name: 'john',
+  lastname: 'smith',
+};
+
+let bb = {
+  name: 'john',
+  lastname: 'smith',
+};
+
+
 
 window.isEqual = isEqual;
 
