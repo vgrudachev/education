a68dc6680af3aedec79b6e2ce16b16a2aa98c8fa
vitaly <vgrudachev.exceedteam@gmail.com>
2020-10-16 15:23:16 +0300

    for review full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,13 @@
-function fullSum(...args) {
-  // Напиши свой код здесь
+function fullSum() {
+  let result = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] === 'number') {
+      result += arguments[i];
+    } else {
+      throw new Error("Wrong Argument Type")
+    };
+  }
+  return result;
 }
 
 window.fullSum = fullSum;
