84307ade477a71d7d863b5456c4b05c7b9b0ea43
vitaly <vgrudachev.exceedteam@gmail.com>
2020-10-16 13:17:34 +0300

    fix sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,16 +1,10 @@
 function sumCheck(a, b) {
-  if (!isNaN(+a) && !isNaN(+b) && typeof a == 'number' && typeof b == 'number') {
-  return a + b;
-  }
+  if (!isNaN(+a) && !isNaN(+b) && typeof a == 'number' && typeof b == 'number') return a + b;
   try {
-    if ( (isNaN(+a) || isNaN(+b)) && typeof a != 'object' && typeof b != 'object') {
-      return NaN;
-    }
+    if ( (isNaN(+a) || isNaN(+b)) && typeof a != 'object' && typeof b != 'object') return NaN;
     throw new Error("Wrong arguments type!");
   } catch(e) {
-    if (e.name == 'Error') {
-      alert(e.message);
-    }
+    if (e.name == 'Error') alert(e.message);
     throw e;
   }
 }
