2a642f6cc182e048535db6168e6b3c44564699df
vitaly <vgrudachev.exceedteam@gmail.com>
2020-10-16 17:00:45 +0300

    copmlite find-pair

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,5 +1,10 @@
 function findPair(arr) {
-  // Напиши свой код здесь
+  for (let i = 0; i <= arr.length; i++) {
+    for (let j = i + 1; j <= arr.length; j++) {
+      if (arr[i] === arr[j]) return arr[i];
+    }
+  }
+  return null;
 };
 
 window.findPair = findPair;
