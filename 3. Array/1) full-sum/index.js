5995df0e32abb1c2288c5a4d17b39f47babd3e50
vitaly <vgrudachev.exceedteam@gmail.com>
2020-10-16 16:36:18 +0300

    fix full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,12 +1,16 @@
-function fullSum() {
-  let result = 0;
+function fullSum(...args) {
   for (let i = 0; i < arguments.length; i++) {
-    if (typeof arguments[i] === 'number') {
-      result += arguments[i];
-    } else {
-      throw new Error("Wrong Argument Type")
-    };
+    if (typeof arguments[i] !== 'number') throw new Error("Wrong Argument Type");
+  };
+
+  let arr = [];
+  for (let i = 0; i < arguments.length; i++) {
+  arr[i] = arguments[i];
   }
+
+  let result = arr.reduce(function(sum, current) {
+    return sum + current;
+  }, 0);
   return result;
 }
 
